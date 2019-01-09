475d9d438f509d4062741efccdde6f10d4e1fb79
Dinesh A <atoliyadinesh87@gmail.com>
2019-01-09 17:16:21 +0530

    Added notification component in root router.

--- src/Routes.js
+++ src/Routes.js
@@ -1,16 +1,20 @@
-import * as React from 'react'
+import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import HomeContainer from './containers/Home'
 import AdminContainer from './containers/Admin'
+import Notification from './components/wrappers/Notification'
 
 class Routes extends React.Component {
   render () {
     return (
-      <Switch>
-        <Route exact path='/' component={HomeContainer} />
-        <Route path='/home' component={HomeContainer} />
-        <Route path='/admin' component={AdminContainer} />
-      </Switch>
+      <React.Fragment>
+        <Switch>
+          <Route exact path='/' component={HomeContainer} />
+          <Route path='/home' component={HomeContainer} />
+          <Route path='/admin' component={AdminContainer} />
+        </Switch>
+        <Notification />
+      </React.Fragment>
     )
   }
 }
--- src/sagas/admin/UpdateSettings.js
+++ src/sagas/admin/UpdateSettings.js
@@ -2,13 +2,14 @@ import { takeLatest, put } from 'redux-saga/effects'
 
 import { UPDATE_ADMIN_SETTINGS_REQUEST } from '../../actions/ActionTypes'
 import { adminSettingsSuccess } from '../../actions/admin/settings'
-import { updateAdminSettingsFailure } from '../../actions/admin/updateSettings'
+import { updateAdminSettingsFailure, updateAdminSettingsSuccess } from '../../actions/admin/updateSettings'
 import adminAPIHelper from '../../services/Admin'
 
 function * startUpdateSettingsFlow (data) {
   try {
     const adminSettings = yield adminAPIHelper.updateAdminSettings(data.payload)
     yield put(adminSettingsSuccess({ response: adminSettings }))
+    yield put(updateAdminSettingsSuccess())
   } catch (err) {
     yield put(updateAdminSettingsFailure({ msg: err }))
   }
