5976dcf434c31f2cc141dc330bdc60cc067fa12d
Dinesh A <atoliyadinesh87@gmail.com>
2019-01-09 15:18:24 +0530

    Added default data.

---
+++ src/data/AdminConfiguration.js
@@ -0,0 +1,14 @@
+const adminConfiguration = {
+  // Currency Rate Refresh Time in seconds 
+  currenciesRatesRefreshTime: 10,
+  // commission in percentage
+  commission: 2,
+  // surcharge in dollar
+  surcharge: 1,
+  // minimal commission in dollar
+  minimalCommission: 1,
+  // Buy/Sell rate margin in percentage
+  rateMargin: 2
+};
+
+export default adminConfiguration;
---
+++ src/data/CurrenciesList.js
@@ -0,0 +1,8 @@
+const currenciesList = {
+  USD: { amount: 1500 },
+  EUR: { amount: 1000 },
+  CAD: { amount: 800 },
+  baseCurrency: 'USD'
+};
+
+export default currenciesList;
