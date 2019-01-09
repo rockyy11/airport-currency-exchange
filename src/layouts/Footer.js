ef7418756cf3cf702d16d509e3b83c0e4c532ae7
Dinesh A <atoliyadinesh87@gmail.com>
2019-01-09 15:19:18 +0530

    Added header and footer with styling.

---
+++ src/layouts/Footer.js
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+const footer = () => (
+  <div className={'footer'}>
+    COPYRIGHT (C) 2019 AIRPORT CURRENCY EXCHANGE SERVICES ALL RIGHTS RESERVED
+  </div>
+)
+
+export default footer
---
+++ src/layouts/Header.js
@@ -0,0 +1,40 @@
+import * as React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const buttonAppBar = () => {
+  return (
+    <React.Fragment>
+      <div className={'rootStyle'}>
+        <div className={'headerLeft'}>
+          Airport Currency Exchange Office
+        </div>
+        <div className={'headerRight'}>
+          <ul>
+            <li>
+              <NavLink
+                to='/home'
+                activeClassName='select'
+                className={'bttn'}>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to='/admin'
+                activeClassName='select'
+                className={'bttn'}>
+                Admin
+              </NavLink>
+            </li>
+            <li> <img
+              className={'logoImage'}
+              src='logo.jpg'
+              alt='logo' /></li>
+          </ul>
+        </div>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default buttonAppBar
