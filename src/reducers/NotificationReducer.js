344f9adf0ec8f45a03ed7368187c04f4cf8ddab1
Dinesh A <atoliyadinesh87@gmail.com>
2019-01-10 16:50:47 +0530

    Notify user whenever user buy or sell the currency.

--- src/reducers/NotificationReducer.js
+++ src/reducers/NotificationReducer.js
@@ -3,6 +3,10 @@ import {
   UPDATE_ADMIN_SETTINGS_FAILURE,
   ADMIN_SETTINGS_FAILURE,
   UPDATE_ADMIN_SETTINGS_SUCCESS,
+  BUY_CURRENCY_FAILURE,
+  SELL_CURRENCY_FAILURE,
+  SELL_CURRENCY_SUCCESS,
+  BUY_CURRENCY_SUCCESS,
   RESET_NOTIFICATION
 } from '../actions/ActionTypes'
 
@@ -13,17 +17,27 @@ const defaultState = {
 
 export default handleActions(
   {
-    [combineActions(UPDATE_ADMIN_SETTINGS_FAILURE, ADMIN_SETTINGS_FAILURE)]: (state, action) => {
+    [combineActions(UPDATE_ADMIN_SETTINGS_FAILURE, ADMIN_SETTINGS_FAILURE, BUY_CURRENCY_FAILURE, SELL_CURRENCY_FAILURE)]: (state, action) => {
       return {
         ...state,
         isError: true,
-        message: action.payload.msg || 'Something went wrong. Please try again later.'
+        message: action.payload.msg || 'Something went wrong. Please try again later !!'
       }
     },
     [UPDATE_ADMIN_SETTINGS_SUCCESS]: (state) => ({
       ...state,
       isError: false,
       message: 'Admin settings have been successfully updated.'
+    }),
+    [BUY_CURRENCY_SUCCESS]: (state, action) => ({
+      ...state,
+      isError: false,
+      message: `Currency ${action.payload.currencyType} has been successfully bought.`
+    }),
+    [SELL_CURRENCY_SUCCESS]: (state, action) => ({
+      ...state,
+      isError: false,
+      message: `Currency ${action.payload.currencyType} has been successfully sold.`
     }),
     [RESET_NOTIFICATION]: (state) => ({
       ...state,
